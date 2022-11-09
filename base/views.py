1b48888062d5698da0df37bd535fe4b5b3f160de
dickson <dkerobean@gmail.com>
2022-11-09 23:52:16 +0000

    - CRUD functionality done

--- base/views.py
+++ base/views.py
@@ -1,7 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RoomForm
+from .models import Room
 
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    room = Room.objects.all()
+    context = {
+        'room':room
+    }
+    return render(request, 'home.html', context)
+
+
+def createRoom(request):
+    form = RoomForm()
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {
+        'form':form
+    }
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance = room)
+
+    if request.method == 'POST':
+        form = room = RoomForm(request.POST, instance = room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {
+        'form':form
+    }
+    return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request,pk):
+    room = Room.objects.get(id=pk)
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    context = {
+        'object':room
+    }
+    return render(request, 'base/delete_form.html', context)
